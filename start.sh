93649148801782e8539db5afd984974d444c25b6
DKhorkov <dkhorkov.dev@gmail.com>
2023-12-07 17:34:04 +0300

    Upgrade bash-script to laucnh on every pc via dynamic searching of maven-repository. also removed unnesessary dir changing

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# shellcheck disable=SC2164
-cd src
+# https://stackoverflow.com/questions/5916157/how-to-get-the-maven-local-repo-location
+mvn_local_repository=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
 
 # Compiling app to ./bin folder
-javac -d ../bin -cp .:/home/dkhorkov/.m2/repository/org/json/json/20220924/json-20220924.jar:\
-/home/dkhorkov/.m2/repository/io/github/cdimascio/dotenv-java/3.0.0/dotenv-java-3.0.0.jar Main.java
+javac -d bin -cp .:${mvn_local_repository}/org/json/json/20220924/json-20220924.jar\
+:${mvn_local_repository}/io/github/cdimascio/dotenv-java/3.0.0/dotenv-java-3.0.0.jar\
+:./src src/Main.java
 
-cd ../
-
-java -cp .:/home/dkhorkov/.m2/repository/org/json/json/20220924/json-20220924.jar\
-:/home/dkhorkov/.m2/repository/io/github/cdimascio/dotenv-java/3.0.0/dotenv-java-3.0.0.jar\
+# Launch compiled app
+java -cp .:${mvn_local_repository}/org/json/json/20220924/json-20220924.jar\
+:${mvn_local_repository}/io/github/cdimascio/dotenv-java/3.0.0/dotenv-java-3.0.0.jar\
 :./bin  Main
